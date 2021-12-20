713d9fec1b766dcf666792bfe629daf0bfbb9e4f
Tilhoo Dheeraj <dti@mac28533.local>
2021-12-20 07:52:50 +0400

    initial commit

---
+++ lib/presentation/screens/walkthrough_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WalkThroughScreen extends StatefulWidget {
+  const WalkThroughScreen({Key? key}) : super(key: key);
+
+  @override
+  _WalkThroughScreenState createState() => _WalkThroughScreenState();
+}
+
+class _WalkThroughScreenState extends State<WalkThroughScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
